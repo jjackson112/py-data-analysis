b4348551051e58ae2ac2238e446e45940c22ce61
jjackson112 <jjackson11211@gmail.com>
2025-05-08 17:47:53 -0400

    Histogram of fare and passengers

--- determining-probability/main.py
+++ determining-probability/main.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open("titanic.csv", "r") as file:
     data = csv.reader(file, delimiter=",")
@@ -19,8 +20,26 @@ for index in range(0, len(fare)):
   else:
       fare_not_survived.append(fare[index])
 
-print(f"The average fare of those who survived was: ${round(np.mean(fare_survived), 2)}")
-print(f"The average fare of those who did not survive was: ${round(np.mean(fare_not_survived), 2)}")
+# print(f"The average fare of those who survived was: ${round(np.mean(fare_survived), 2)}")
+# print(f"The average fare of those who did not survive was: ${round(np.mean(fare_not_survived), 2)}")
 
-print(f"The median fare of those who survived was: ${round(np.median(fare_survived), 2)}")
-print(f"The median fare of those who did not survive was: ${round(np.median(fare_not_survived), 2)}")
+# print(f"The median fare of those who survived was: ${round(np.median(fare_survived), 2)}")
+# print(f"The median fare of those who did not survive was: ${round(np.median(fare_not_survived), 2)}")
+
+# use range() to add bins - 15
+# start at 0 and no passenger paid more than $240
+bins = range(0,240,15)
+
+# alpha value determines the transparency of the bars
+plt.hist(fare_not_survived, bins, histtype="bar", color="red", alpha=0.5)
+plt.hist(fare_survived, bins, histtype="bar", color="orange", alpha=0.5)
+
+# no one paid more than $240 and the # of people who died don't exceed 360
+plt.xticks(range(0, 240, 20))
+plt.yticks(range(0, 360,25))
+
+plt.xlabel("Fare")
+plt.ylabel("Passengers")
+
+plt.title("Summarizing the Titanic Survival Data")
+plt.savefig("determining_probability.png")
