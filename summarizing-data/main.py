7bff773b90858f24775934a6e72fccd1ca665e95
jjackson112 <jjackson11211@gmail.com>
2025-05-08 16:49:48 -0400

    Print avg and median fares of survived + deceased

--- summarizing-data/main.py
+++ summarizing-data/main.py
@@ -19,5 +19,8 @@ for index in range(0, len(fare)):
   else:
     fare_not_survived.append(fare[index])
 
-print(fare_survived)
-print(fare_not_survived)
+print(f"The average fare of the survived is {round(np.mean(fare_survived), 2)}")
+print(f"The average fare of the deceased is {round(np.mean(fare_not_survived), 2)}")
+
+print(f"The median fare of the survived is {round(np.median(fare_survived), 2)}")
+print(f"The median fare of the deceased is {round(np.median(fare_not_survived), 2)}")
