2e12db925843868dcb461bbd3f9f4fada913abc5
jjackson112 <jjackson11211@gmail.com>
2025-05-08 12:49:58 -0400

    Data aggregation

--- tips-and-group-size/main.py
+++ tips-and-group-size/main.py
@@ -6,3 +6,18 @@ with open("tips.csv", "r") as file:
   headers = next(data)
   data_list = list(data)
   data_numpy = np.array(data_list)
+
+# get the size of your parties with data_numpy
+# select each row with : 
+# grab the index position (the 7th column of dataset)
+size = data_numpy[:,6]
+
+# pass data_numpy in NumPy array, select each row and grab the index position
+# dtype=float makes the array float because the csv data is in a string - data type
+# use tips data to perform calculations
+tips = np.array(data_numpy[:, 1], dtype=float)
+
+# 0 index position pulls the total_bill column
+bills = np.array(data.numpy[:, 0], dtype=float)
+
+tip_percentages = tips/bills
