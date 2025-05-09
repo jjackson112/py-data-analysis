4a05db60c9d3fdc13467de02f6b69ee1af18bc1d
jjackson112 <jjackson11211@gmail.com>
2025-05-09 10:21:33 -0400

    Determining a realationship - pandas

--- determining-a-relationship/main.py
+++ determining-a-relationship/main.py
@@ -32,3 +32,22 @@ print(wage_usd)
 wage_and_happiness = wage.merge(happiness)
 print(wage_and_happiness)
 
+# group merged wage_and_happiness with groupby()
+wage_and_happiness_by_country = (wage_and_happiness.groupby("Country").sum())
+
+# calculate the mean for the Value series in the dataframe
+wage_average_per_country = wage_and_happiness["Value"].mean()
+
+happiness_average_per_country = wage_and_happiness_by_country["Happiness score"].mean()
+
+# Pull 10 countries with the highest average wages
+print(f"Countries with largest average wages: ", wage_average_per_country.nlargest(10))
+
+# Pull 10 countries with the smallest average wages
+print(f"Countries with the smallese average wages: ", wage_average_per_country.nsmallest(10))
+
+# Pull 10 countries with the highest happiness values 
+print(f"Countries with the highest happiness levels: ", happiness_average_per_country.nlargest(10))
+
+# Pull 10 countries with the lowest happiness values 
+print(f"Countries with the lowest happiness levels: ", happiness_average_per_country.nsmallest(10))
