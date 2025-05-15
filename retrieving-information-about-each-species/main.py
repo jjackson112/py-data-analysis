9eccc3502e0b0502d11f026c94f226fccf998fbb
jjackson112 <jjackson11211@gmail.com>
2025-05-15 09:25:08 -0400

    Retrieve data from table

--- retrieving-information-about-each-species/main.py
+++ retrieving-information-about-each-species/main.py
@@ -18,6 +18,23 @@ def get_categories(url):
     data[category_name] = category_animals
   return data
 
+# this function will extract the class of each animal
+def get_animal(url):
+  soup = get_soup(url)
+  table = soup.find("table", {"class": "infobox biota"})
+  if not table:
+    return "No class found."
+  rows = table.find_all("tr")
+# if Class is found, then get the contents of the link inside that row and save it to animal_class
+  for row in rows:
+    if "Class:" in row.get_text():
+      animal_class = row.find("a").contents[0]
+  return animal_class
+
+
 category_data = get_categories("https://skillcrush.github.io/web-scraping-endangered-species/")
 
-print(category_data)
+# use get_animal function to extract the data
+animal_class = get_animal("https://en.wikipedia.org/wiki/Honey_badger")
+
+print(animal_class)
