fd2fff23d0844dc6d6b3841867560304dcd698b4
jjackson112 <jjackson11211@gmail.com>
2025-05-19 19:29:44 -0400

    mosaic plot

--- making-a-mosaic-plot/main.py
+++ making-a-mosaic-plot/main.py
@@ -1,10 +1,39 @@
 import re
 import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.graphics.mosaicplot import mosaic
 
 with open("data.json", "r") as text:
     data = json.load(text)
 
 for item in data:
-  item["Category"] = re.compile(" [\.(]").split(item["Category"])[0]
+  item["Category"] = re.compile("[\.(]").split(item["Category"])[0]
 
-print(data)
+# mosaic plot will show data from these 3 classes
+classes = ["Mammalia", "Aves", "Reptilia"]
+# will show only these conservation statuses
+statuses = ["Endangered", "Critically endangered", "Vulnerable"]
+
+mosaic_data = []
+
+# will only extract data from those classes and conservation statuses
+# search for values of each item with a specific class key and category key
+for item in data:
+   if item["Animal Class"] in classes and item["Category"] in statuses:
+    mosaic_data.append(item)
+
+properties = {
+   "Endangered": {"color": "#facdb6"},
+   "Critically endangered": {"color": "#c5cade"},
+   "Vulnerable": {"color": "#a8dbd2"}
+}
+
+plt.rc("font", size=8)
+
+# create a dataframe
+mosaic_dataframe = pd.DataFrame(mosaic_data)
+
+fig = mosaic(mosaic_dataframe, ["Category", "Animal Class"], title="Conservation Status by Animal Class", gap=[0.02, 0.02], axes_label=True, properties=lambda x: properties[x[0]])
+
+plt.savefig("mosaic.png")
